f2ec8303cf11ef9d8477d29472997fc2438fac1c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2025-06-21 09:12:00 +0300

    Fix comments a bit

--- crates/farmer/ab-farmer-components/src/proving.rs
+++ crates/farmer/ab-farmer-components/src/proving.rs
@@ -245,8 +245,8 @@ where
             drop(sector_record_chunks);
 
             // TODO: This is a workaround for https://github.com/rust-lang/rust/issues/139866 that
-            //  allows the code to compile. Constant 16 is hardcoded here and in `if` branch below
-            //  for compilation to succeed
+            //  allows the code to compile. Constant 65536 is hardcoded here and below for
+            //  compilation to succeed.
             const _: () = {
                 assert!(Record::NUM_S_BUCKETS == 65536);
             };
