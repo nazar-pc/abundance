29bf05699a974fdef5a232be2e9cfff6b6036f53
nazar-pc <nazar@mokrynskyi.com>
2025-06-21 13:24:11 +0000

    Deploying to gh-pages from @ nazar-pc/abundance@982ea07d8eedf23132de14f33bf57525029cfc19 🚀

---
+++ rust-docs/trait.impl/ab_contracts_standards/tx_handler/trait.TxHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ab_contracts_test_utils",[["impl TxHandler for <a class=\"struct\" href=\"ab_contracts_test_utils/dummy_wallet/struct.DummyWallet.html\" title=\"struct ab_contracts_test_utils::dummy_wallet::DummyWallet\">DummyWallet</a>"]]],["ab_example_contract_wallet",[["impl TxHandler for <a class=\"struct\" href=\"ab_example_contract_wallet/struct.ExampleWallet.html\" title=\"struct ab_example_contract_wallet::ExampleWallet\">ExampleWallet</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[226,215]}
