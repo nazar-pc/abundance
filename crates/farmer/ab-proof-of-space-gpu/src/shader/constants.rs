e9b4ad122de6ded8677afbb99bde13dbeefefa0b
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2025-07-01 18:50:43 +0300

    Upgrade `rust-gpu` to support better handling of constants

--- crates/farmer/ab-proof-of-space-gpu/src/shader/constants.rs
+++ crates/farmer/ab-proof-of-space-gpu/src/shader/constants.rs
@@ -1,3 +1,10 @@
+// TODO: Replace this constant with usage of `PosProof::K` after
+//  https://github.com/Rust-GPU/rust-gpu/pull/249 is merged
+pub(super) const K: u8 = 20;
+#[cfg(not(target_arch = "spirv"))]
+const _: () = {
+    assert!(K == ab_core_primitives::pos::PosProof::K);
+};
 /// PRNG extension parameter to avoid collisions
 pub(super) const PARAM_EXT: u8 = 6;
 // pub(super) const PARAM_M: u16 = 1 << PARAM_EXT;
