aba66f25b92ac0e1e2140bb9b705e6e97ceeef6a
nazar-pc <nazar@mokrynskyi.com>
2025-10-07 13:42:18 +0000

    Deploying to gh-pages from @ nazar-pc/abundance@dc19a9c316b67df330f15864b6b698f71ba2219e 🚀

---
+++ rust-docs/trait.impl/ab_contracts_common/trait.ContractTraitDefinition.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ab_contracts_standards",[["impl ContractTraitDefinition for dyn <a class=\"trait\" href=\"ab_contracts_standards/fungible/trait.Fungible.html\" title=\"trait ab_contracts_standards::fungible::Fungible\">Fungible</a>"],["impl ContractTraitDefinition for dyn <a class=\"trait\" href=\"ab_contracts_standards/tx_handler/trait.TxHandler.html\" title=\"trait ab_contracts_standards::tx_handler::TxHandler\">TxHandler</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[421]}
