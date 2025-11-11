e5b33cf678a523fd162aeafae090ccc8056dde50
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2025-11-12 00:25:32 +0200

    Do actual unchecked array access in the polyfill

--- crates/farmer/ab-proof-of-space-gpu/src/shader/polyfills.rs
+++ crates/farmer/ab-proof-of-space-gpu/src/shader/polyfills.rs
@@ -1,3 +1,6 @@
+#[cfg(target_arch = "spirv")]
+use spirv_std::arch::IndexUnchecked;
+
 // TODO: This is a polyfill to work around for this issue:
 //  https://github.com/Rust-GPU/rust-gpu/issues/241#issuecomment-3005693043
 #[cfg(target_arch = "spirv")]
@@ -12,11 +15,11 @@ pub(super) trait ArrayIndexingPolyfill<T> {
 impl<const N: usize, T> ArrayIndexingPolyfill<T> for [T; N] {
     #[inline(always)]
     unsafe fn get_unchecked(&self, index: usize) -> &T {
-        &self[index]
+        unsafe { self.index_unchecked(index) }
     }
 
     #[inline(always)]
     unsafe fn get_unchecked_mut(&mut self, index: usize) -> &mut T {
-        &mut self[index]
+        unsafe { self.index_unchecked_mut(index) }
     }
 }
