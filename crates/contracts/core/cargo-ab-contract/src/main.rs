f2e57ef3688feb51230f3b67e41a26d03455f12c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2025-12-29 06:40:06 +0200

    Implement `cargo-ab-contract build` command, remove x86-64 target specification file

--- crates/contracts/core/cargo-ab-contract/src/main.rs
+++ crates/contracts/core/cargo-ab-contract/src/main.rs
@@ -1,22 +1,40 @@
 #![feature(try_blocks)]
 
 mod convert;
+mod target_specification;
 
 use crate::convert::convert;
+use crate::target_specification::TargetSpecification;
 use ab_cli_utils::init_logger;
 use ab_contract_file::ContractFile;
 use anyhow::Context;
+use cargo_metadata::MetadataCommand;
 use clap::Parser;
 use std::env;
 use std::fs::{read, write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use std::process::Command;
 
 /// Cargo extension for working with Abundance contracts
 #[derive(Debug, Parser)]
 #[clap(about, version)]
-enum Command {
-    /// Compile Rust contract using simple CLI
-    Build,
+enum Cli {
+    /// Write and print a path to the target specification JSON file
+    TargetSpecPath,
+    /// Compile a contract using simple CLI.
+    ///
+    /// Note that unoptimized builds are not supported, hence `release` by default.
+    Build {
+        /// Package to build
+        #[arg(long, short = 'p')]
+        package: Option<String>,
+        /// Comma separated list of features to activate
+        #[arg(long)]
+        features: Option<String>,
+        /// Build artifacts with the specified profile
+        #[arg(long, default_value = "release")]
+        profile: String,
+    },
     /// Convert `.contract.so` ELF file to `.contract` for execution environment
     Convert {
         /// Input file with `.contract.so` extension
@@ -55,11 +73,110 @@ pub fn main() -> anyhow::Result<()> {
         args
     });
 
-    match command {
-        Command::Build => {
-            unimplemented!("Building contracts is not implemented yet");
+    match cli {
+        Cli::TargetSpecPath => {
+            let target_specification = TargetSpecification::create()?;
+
+            println!("{}", target_specification.path().display());
+
+            Ok(())
         }
-        Command::Convert {
+        Cli::Build {
+            package,
+            features,
+            profile,
+        } => {
+            let target_specification = TargetSpecification::create()?;
+
+            let mut command_builder = Command::new("cargo");
+            command_builder.args([
+                "rustc",
+                "-Z",
+                "build-std=core",
+                "--crate-type",
+                "cdylib",
+                "--target",
+                target_specification
+                    .path()
+                    .to_str()
+                    .context("Path to target specification file is not valid UTF-8")?,
+            ]);
+
+            if let Some(package) = &package {
+                command_builder.args([
+                    "--package",
+                    package,
+                    "--features",
+                    &format!("{package}/guest"),
+                ]);
+            } else {
+                command_builder.args(["--features", "guest"]);
+            }
+            if let Some(features) = features {
+                command_builder.args(["--features", &features]);
+            }
+
+            command_builder.args(["--profile", &profile]);
+
+            let metadata = MetadataCommand::new()
+                .exec()
+                .expect("Failed to fetch cargo metadata");
+
+            let cdylib_path = metadata
+                .target_directory
+                .as_std_path()
+                .join("riscv64em-unknown-none-abundance")
+                .join(&profile)
+                .join({
+                    let package_name = if let Some(package) = &package {
+                        package
+                    } else {
+                        let current_dir =
+                            env::current_dir().context("Failed to get current directory")?;
+                        let current_manifest = current_dir.join("Cargo.toml");
+                        metadata
+                            .packages
+                            .iter()
+                            .find_map(|package| {
+                                if package.manifest_path == current_manifest {
+                                    Some(&package.name)
+                                } else {
+                                    None
+                                }
+                            })
+                            .context("Failed to find package name")?
+                    };
+
+                    format!("{}.contract.so", package_name.replace('-', "_"))
+                });
+
+            println!("Building ELF cdylib at {}", cdylib_path.display());
+            println!("Command:");
+            println!("  {:?}", command_builder);
+
+            let status = command_builder
+                .status()
+                .context("Failed to build a contract")?;
+
+            if !status.success() {
+                return Err(anyhow::anyhow!("Failed to build a contract"));
+            }
+
+            let contract_path = cdylib_path.with_extension("");
+
+            println!("Converting to `.contract` at {}", contract_path.display());
+
+            let input_bytes = read(cdylib_path).context("Failed to read input file")?;
+            let output_bytes = convert(&input_bytes)?;
+            ContractFile::parse(&output_bytes, |_| Ok(()))
+                .context("Failed to parse converted contract file")?;
+            write(contract_path, output_bytes).context("Failed to write output file")?;
+
+            println!("Build successful");
+
+            Ok(())
+        }
+        Cli::Convert {
             input_file,
             output_file,
         } => {
@@ -74,14 +191,14 @@ pub fn main() -> anyhow::Result<()> {
             println!("Conversion successful");
             Ok(())
         }
-        Command::Verify { file } => {
+        Cli::Verify { file } => {
             println!("Verifying {}", file.display());
             ContractFile::parse(&read(file)?, |_| Ok(()))
                 .context("Failed to parse contract file")?;
             println!("Verification successful");
             Ok(())
         }
-        Command::Recover { .. } => {
+        Cli::Recover { .. } => {
             unimplemented!("Recovering of ELF files is not implemented yet");
         }
     }
