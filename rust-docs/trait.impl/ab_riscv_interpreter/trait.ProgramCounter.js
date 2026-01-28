7a4e0a491a1ed9925440683ac56cfedb7025de8b
nazar-pc <nazar@mokrynskyi.com>
2026-01-28 03:54:07 +0000

    Deploying to gh-pages from @ nazar-pc/abundance@da20c371d65796922a4ea0a239f5453d7bcd4e06 🚀

---
+++ rust-docs/trait.impl/ab_riscv_interpreter/trait.ProgramCounter.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["ab_riscv_benchmarks",[["impl&lt;Memory&gt; <a class=\"trait\" href=\"ab_riscv_interpreter/trait.ProgramCounter.html\" title=\"trait ab_riscv_interpreter::ProgramCounter\">ProgramCounter</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>, Memory, &amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"ab_riscv_benchmarks/host_utils/struct.EagerTestInstructionFetcher.html\" title=\"struct ab_riscv_benchmarks::host_utils::EagerTestInstructionFetcher\">EagerTestInstructionFetcher</a><div class=\"where\">where\n    Memory: <a class=\"trait\" href=\"ab_riscv_interpreter/trait.VirtualMemory.html\" title=\"trait ab_riscv_interpreter::VirtualMemory\">VirtualMemory</a>,</div>",0]]],["ab_riscv_interpreter",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[824,28]}
---
+++ rust-docs/trait.impl/ab_riscv_interpreter/trait.VirtualMemory.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["ab_riscv_benchmarks",[["impl&lt;const MEMORY_SIZE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"ab_riscv_interpreter/trait.VirtualMemory.html\" title=\"trait ab_riscv_interpreter::VirtualMemory\">VirtualMemory</a> for <a class=\"struct\" href=\"ab_riscv_benchmarks/host_utils/struct.TestMemory.html\" title=\"struct ab_riscv_benchmarks::host_utils::TestMemory\">TestMemory</a>&lt;MEMORY_SIZE&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[489]}
