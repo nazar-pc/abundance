ea39b37da4ab51eeb994eb21d3d37a4cfe85570f
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2026-05-30 12:18:39 +0300

    Remove `generic_const_exprs` usage from most other crates

--- crates/farmer/ab-farmer-components/src/lib.rs
+++ crates/farmer/ab-farmer-components/src/lib.rs
@@ -11,10 +11,6 @@
     portable_simd,
     try_blocks
 )]
-#![expect(incomplete_features, reason = "generic_const_exprs")]
-// TODO: This feature is not actually used in this crate, but is added as a workaround for
-//  https://github.com/rust-lang/rust/issues/141492
-#![feature(generic_const_exprs)]
 #![warn(rust_2018_idioms, missing_debug_implementations, missing_docs)]
 
 pub mod auditing;
